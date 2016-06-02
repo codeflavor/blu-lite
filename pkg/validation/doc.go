706e7daf929b4e712adc23920575dc00c37b9279
PI-Victor <ipalade@redhat.com>
2016-06-02 21:36:49 +0200

    add packages for services and validation

---
+++ pkg/validation/doc.go
@@ -0,0 +1,2 @@
+// Package validation contains the necessary functionality to perform cross application validation for different services before they start as well as validation for the configuration that is passed to the application.
+package validation
