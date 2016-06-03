6432986ca68988c38d54cb91bdd9b2905592e03f
PI-Victor <ipalade@redhat.com>
2016-06-03 16:06:50 +0200

    add react login template
    * add custom styling

--- assets/src/blulite.js
+++ assets/src/blulite.js
@@ -1,14 +1,43 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 
-var BluLIte = React.createClass({
+var BluLite = React.createClass({
   render: function () {
     return (
-      <h1>Hello World!</h1>
+      <div>
+        <Menu/>
+        <Login/>
+      </div>
+    )
+  }
+})
+
+var Menu = React.createClass({
+  render: function () {
+    return (
+      <div className='ui secondary menu'>
+        <a className='item'>First Item</a>
+      </div>
+    )
+  }
+})
+
+var Login = React.createClass({
+  render: function () {
+    return (
+      <form id='login-form' className='ui form'>
+        <div className='field'>
+          <input type='text' placeholder='User name'/>
+        </div>
+        <div className='field'>
+          <input type='text' placeholder='Password'/>
+        </div>
+        <div className='ui fluid blue submit button'>Submit</div>
+      </form>
     )
   }
 })
 
 ReactDOM.render(
-  <BluLIte />, document.getElementById('test')
+  <BluLite/>, document.getElementById('blulite')
 )
