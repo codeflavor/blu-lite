cee943ccbaadfb94e34ea2f647a231c4938f73a0
Victor <ipalade@redhat.com>
2016-09-11 12:03:24 +0200

    remove unused files and dirs

--- pkg/validation/doc.go
+++
@@ -1,2 +0,0 @@
-// Package validation contains the necessary functionality to perform cross application validation for different services before they start as well as validation for the configuration that is passed to the application.
-package validation
