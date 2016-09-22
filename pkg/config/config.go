75197fc560694b20b70ea22df9596fd42dc52002
Victor Palade <palade.ionut@gmail.com>
2016-09-22 14:36:58 +0200

    rearrange config into its own package

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,9 +1,5 @@
 package config
 
-import (
-	"gopkg.in/mgo.v2"
-)
-
 type dbDriver string
 
 // AppConfig holds the application configuration and also encompasses the
@@ -15,25 +11,26 @@ type AppConfig struct {
 	LogLevel string   `json:"loglevel"`
 }
 
+// dbConfig holds information that is necessary to connect to a mongodb
+// instance.
 type dbConfig struct {
-	UserName string    `json:"username"`
-	Password string    `json:"password"`
-	URL      string    `json:"URL"`
-	Driver   dbDriver  `json:"driver"`
-	Session  dbSession `json:"session"`
-}
-
-// DbHandler holds the active connection to the database
-type dbSession struct {
-	mongoSession *mgo.Session
+	UserName string `json:"username"`
+	Password string `json:"password"`
+	URL      string `json:"URL"`
 }
 
 // newAppConfig is a helper function for instantiating a new application
-// configuration
-func newAppConfig() *AppConfig {
-	return &AppConfig{}
+// configuration.
+func newAppConfig(workDir, confFile, loglevel string, dbConf *dbConfig) *AppConfig {
+	return &AppConfig{
+		WorkDir:  workDir,
+		ConfFile: confFile,
+		DbConf:   *dbConf,
+		LogLevel: loglevel,
+	}
 }
 
+// newDbConfig holds the information that is necessary to connect to mongodb.
 func newDbConfig(user, pass, mongodbURL string) *dbConfig {
 	return &dbConfig{
 		UserName: user,
@@ -42,6 +39,8 @@ func newDbConfig(user, pass, mongodbURL string) *dbConfig {
 	}
 }
 
+// generateDefaults generates a default configuration for the application when
+// no application configuration file was specified by the user.
 func (app *AppConfig) generateDefaults() error {
 	return nil
 }
--- pkg/doc.go
+++ pkg/doc.go
@@ -1,3 +1,3 @@
-// Package pkg contains all the related libraries that make up the Blue Lite
+// Package pkg contains all the related libraries that make up the ServOps
 // API.
 package pkg
