6bc53a3820e608e0d1d3787d0a48b4a73bab30b4
Victor Palade <palade.ionut@gmail.com>
2016-11-21 00:25:54 +0100

    remove blue lite ref

--- cmd/servops/main.go
+++ cmd/servops/main.go
@@ -6,11 +6,6 @@ import (
 	"github.com/codeflavor/servops/pkg/cmd"
 )
 
-func main() {
-	RootCmd.AddCommand(cmd.StartCommand)
-	RootCmd.Execute()
-}
-
 // RootCmd prints instructions about how to start the API Server.
 var RootCmd = &cobra.Command{
 	Use:   "servops",
@@ -19,15 +14,15 @@ var RootCmd = &cobra.Command{
 Specify the application working directory with servops start
 --workdir=/path/to/dir. The application will try to find and validate the
 config.yml file in the specified working directory. If validation fails, it
-will exist with an error. If no config file is found, it will create one with
+will exist with an error. If no config file is found, it will generate one with
 defaults.
-
-You can start a new instance of the API Server by specyfing a config file
-with bluelite start --config-file=/path/to/config/file.
-If you don't, the application will create default settings and store them in
-its working directory.
 `,
 	Run: func(command *cobra.Command, args []string) {
 		command.Help()
 	},
+}
+
+func main() {
+	RootCmd.AddCommand(cmd.StartCommand)
+	RootCmd.Execute()
 }
