604538f7c356ea64168c5dafa873aa759055c1c0
Victor Palade <palade.ionut@gmail.com>
2016-11-27 17:29:44 +0100

    fix displaying of errors

--- pkg/cmd/start/validation.go
+++ pkg/cmd/start/validation.go
@@ -18,16 +18,15 @@ func ValidateUserConfig(confFile, workDir, logLevel string) {
 	// TODO: refactor this whenever possible
 	if confFile != "" {
 		glog.V(0).Infof("Trying to load user specified configuration from %s...", confFile)
-		// NOTE: this craps is scoped
 		appConfig, err = loadUserConfig(confFile)
 		if err != nil {
-			log.Fatalf("error: An error occured while loading the configuration file %s (%v)", confFile, err)
+			log.Fatalf("error: An error occured while loading the configuration file %s: (%v)", confFile, err)
 		}
 	} else {
 		glog.V(0).Info("No user specified configuration, generating defaults...")
-		appConfig, err = generateDefaultConfig(workDir)
+		appConfig, err = genDefaultConf(workDir)
 		if err != nil {
-			log.Fatalf("error: An error occured while generating a default config for the application %v ", err)
+			log.Fatalf("error: An error occured while generating a default config: %v ", err)
 		}
 		glog.V(0).Info("Configuration loaded, trying to start service...")
 	}
